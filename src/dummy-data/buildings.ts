beb7f21e7349100d6245eba8ad0b12643cb8b5f6
josejulio11 <jose.sanchez25@upr.edu>
2018-12-01 14:22:06 -0800

    Added new building: Enfermeria and Natatorio

--- src/dummy-data/buildings.ts
+++ src/dummy-data/buildings.ts
@@ -15,10 +15,30 @@ export const Buildings: Array<Building> = [
       id: 29,
       name: "Edificio Luis Monzon",
       abbreviation: "M",
-      category: [],
+      category: [Category.Academic],
       short_description: "Departamento de Matematicas",
       long_description: "",
       coordinates: [1,0],
       image: '../../assets/img/edificios/edificio_monzon.jpg'
     },
+    {
+      id: 28,
+      name: "Edificio Josefina Torres Torres",
+      abbreviation: "EE",
+      category: [Category.Academic],
+      short_description: "Departamento de Enfermeria",
+      long_description: "",
+      coordinates: [0.5,0.5],
+      image: '../../assets/img/edificios/edificio_enfermeria.jpg'
+    },
+    {
+      id: 27,
+      name: "Complejo Natatorio",
+      abbreviation: "N",
+      category: [Category.Academic, Category.Sports],
+      short_description: "Complejo Natatorio",
+      long_description: "",
+      coordinates: [0,1],
+      image: '../../assets/img/edificios/natatorio.jpeg'
+    }
 ];
